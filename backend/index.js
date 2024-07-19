7a33d8df3905a60bf44fa0cb34cbdb0d90ea93c7
Sparsh Bhardwaj <118371274+Sparsh-Bdwj@users.noreply.github.com>
2024-07-19 20:21:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["https://stalwart-belekoy-d5213a.netlify.app"],
+    origin: ["https://gorgeous-sundae-29396d.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
